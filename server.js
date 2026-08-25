672ac61d64050a1cabf15bd63d0c2e04213e3497
Nayesz <69222696+Nayesz@users.noreply.github.com>
2026-08-25 16:30:58 -0300

    agrego nuevas rutas al servidor y archivo de prueba

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 const { createServer } = require('node:http');
 
 const port = 3000;
-const hostname =  '0.0.0.0';
+const hostname = '0.0.0.0';
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+
+
+    if (req.method === 'GET' && req.url === '/') {
+        /*
+         * GET /: responde con status code 200 y un mensaje simple en texto plano, confirmando que el servidor está funcionando.
+         */
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hola, el servidor está funcionando! :)');
+        return;
+    } else if (req.method === 'POST' && req.url === '/archivo') {
+        /*
+         * POST /archivo: recibe un archivo (o cualquier contenido binario) en el body del request,
+         * cuenta la cantidad total de bytes recibidos en el payload, y responde en texto plano con ese número. 
+         */
+        let totalBytes = 0; // Seteamos el contador de bytes 
+
+        req.on('data', (chunk) => { // cada vez que llega el evento 'data' se suma al contador de bytes el largo del chunk.
+            totalBytes += chunk.length;
+        });
+
+    req.on('end', () => {
+        res.statusCode = 200; // responde cuando el evento end confirme que no va a llegar más contenido.
+        res.setHeader('Content-Type', 'text/plain');
+        res.end(`Cantidad de bytes recibidos: ${totalBytes}`);
+    });
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Ruta no encontrada.');
+    }
+
 });
+
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
